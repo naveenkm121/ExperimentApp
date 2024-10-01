626c965041d1c0d1e6539a8856a9f941007a89a6
naveenkm121 <naveeniitrpr100@gmail.com>
2024-10-01 17:27:50 +0530

    chnages

--- app/src/main/java/com/ecommerce/experimentapp/ui/ContactActivity.kt
+++ app/src/main/java/com/ecommerce/experimentapp/ui/ContactActivity.kt
@@ -37,13 +37,21 @@ class ContactActivity : AppCompatActivity() {
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
         setupRecyclerView()
-        getContactList(2)
+        searchContact()
 
+
+    }
+
+    private fun searchContact() {
+        binding.contentContact.submitBTN.setOnClickListener {
+            val inputValue = binding.contentContact.inputET.text.toString()
+            getContactList(inputValue.toInt())
+        }
     }
 
     private fun setupRecyclerView() {
         adapter = ContactAdapter()
-        binding.contentContact.recyclerView.layoutManager=LinearLayoutManager(baseContext)
+        binding.contentContact.recyclerView.layoutManager = LinearLayoutManager(baseContext)
         binding.contentContact.recyclerView.adapter = adapter
     }
 
@@ -55,22 +63,31 @@ class ContactActivity : AppCompatActivity() {
         call.enqueue(object : Callback<ContactReq> {
             override fun onResponse(call: Call<ContactReq>, response: Response<ContactReq>) {
                 if (response.isSuccessful) {
-                    var contactReq : ContactReq? = response.body()
-                    if(contactReq!=null) {
+                    var contactReq: ContactReq? = response.body()
+                    if (contactReq != null) {
                         contactDataList = contactReq.contacts as ArrayList<ContactData>
+                        contactDataList.sortBy {  it.name}
                         adapter.setItem(contactDataList)
                     }
 
                     DebugHandler.log("get Contacts list successfully from server ")
-                    Toast.makeText(baseContext, "get Contacts list successfully from server", Toast.LENGTH_LONG).show()
+                    Toast.makeText(
+                        baseContext,
+                        "get Contacts list successfully from server",
+                        Toast.LENGTH_LONG
+                    ).show()
                 } else {
-                    DebugHandler.log( "Upload failed: ${response.errorBody().toString()}")
-                    Toast.makeText(baseContext, "Failed to get  Contacts from server: ${response.errorBody().toString()}", Toast.LENGTH_LONG).show()
+                    DebugHandler.log("Upload failed: ${response.errorBody().toString()}")
+                    Toast.makeText(
+                        baseContext,
+                        "Failed to get  Contacts from server: ${response.errorBody().toString()}",
+                        Toast.LENGTH_LONG
+                    ).show()
                 }
             }
 
             override fun onFailure(call: Call<ContactReq>, t: Throwable) {
-                DebugHandler.log( "Error: ${t.message}")
+                DebugHandler.log("Error: ${t.message}")
             }
         })
     }
